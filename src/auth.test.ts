4b5f07b88f8f72a2632a8a1e4113bc23701da9b8
ajrollerson <alistair.jrollerson@hotmail.co.uk>
2026-08-17 12:35:17 +0700

    Add tests, update ci.yml

---
+++ src/auth.test.ts
@@ -0,0 +1,29 @@
+import { describe, expect, test } from "vitest";
+import { IncomingHttpHeaders } from "http";
+import { getAPIKey } from "./api/auth.js";
+
+const nullAuth: IncomingHttpHeaders = {
+    "potato": undefined
+}
+
+const incorrectAuth: IncomingHttpHeaders = {
+    "authorization": "Basic QWxhZGRpbjpPcGVuU2VzYW1l"
+}
+
+const correct: IncomingHttpHeaders = {
+  "authorization": "ApiKey QWxhZGRpbjpPcGVuU2VzYW1l",
+};
+
+describe("authorization", () => {
+  test("null header", () => {
+    expect(getAPIKey(nullAuth)).toBeNull();
+  });
+
+  test("malformed auth header", () => {
+    expect(getAPIKey(incorrectAuth)).toBeNull();
+  });
+
+  test("Correct auth header", () => {
+    expect(getAPIKey(correct)).toBe("QWxhZGRpbjpPcGVuU2VzYW1l");
+  });
+});
